4144b29eb3ee19779c228fb62aac4521a3228e0a
M VEERA VENKATA SURYA KANDIPALLI <131734149+Surya9656@users.noreply.github.com>
2026-08-03 15:56:44 +0530

    Add outputs for ECS and application log group names

--- infra/modules/cloudwatch/variables.tf
+++ infra/modules/cloudwatch/variables.tf
@@ -1,1 +1,9 @@
+output "ecs_log_group_name" {
+  description = "ECS CloudWatch log group name"
+  value       = aws_cloudwatch_log_group.ecs.name
+}
 
+output "application_log_group_name" {
+  description = "Application CloudWatch log group name"
+  value       = aws_cloudwatch_log_group.application.name
+}
