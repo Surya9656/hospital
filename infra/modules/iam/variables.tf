d6ed680bdcf33adfbc42c9f40abca4fa4b35b811
M VEERA VENKATA SURYA KANDIPALLI <131734149+Surya9656@users.noreply.github.com>
2026-08-04 15:48:03 +0530

    Create variables.tf

---
+++ infra/modules/iam/variables.tf
@@ -0,0 +1,14 @@
+variable "project_name" {
+  description = "Project name"
+  type        = string
+}
+
+variable "environment" {
+  description = "Deployment environment"
+  type        = string
+}
+
+variable "github_repository" {
+  description = "GitHub repository in owner/repository format"
+  type        = string
+}
