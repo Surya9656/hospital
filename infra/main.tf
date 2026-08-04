d489c9710bbae919df7a0c5711dd008887434615
M VEERA VENKATA SURYA KANDIPALLI <131734149+Surya9656@users.noreply.github.com>
2026-08-04 15:26:50 +0530

    Delete infra directory

--- infra/main.tf
+++
@@ -1,7 +0,0 @@
-module "cloudwatch" {
-  source = "./modules/cloudwatch"
-
-  project_name       = var.project_name
-  environment        = var.environment
-  log_retention_days = 30
-}
--- infra/modules/cloudwatch/variables.tf
+++
@@ -1,9 +0,0 @@
-output "ecs_log_group_name" {
-  description = "ECS CloudWatch log group name"
-  value       = aws_cloudwatch_log_group.ecs.name
-}
-
-output "application_log_group_name" {
-  description = "Application CloudWatch log group name"
-  value       = aws_cloudwatch_log_group.application.name
-}
