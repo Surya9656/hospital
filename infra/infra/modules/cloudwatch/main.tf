53033cbca1f2cbcfb7cb18fe257403acc9a63461
M VEERA VENKATA SURYA KANDIPALLI <131734149+Surya9656@users.noreply.github.com>
2026-08-04 15:32:02 +0530

    Delete infra/infra directory

--- infra/infra/modules/cloudwatch/main.tf
+++
@@ -1,4 +0,0 @@
-resource "aws_cloudwatch_log_group" "this" {
-  name              = "/ecs/${var.project_name}-${var.environment}"
-  retention_in_days = 30
-}
--- infra/infra/modules/route53/outputs.tf
+++
@@ -1,7 +0,0 @@
-output "domain_name" {
-  value = var.domain_name
-}
-
-output "application_url" {
-  value = "https://${var.domain_name}"
-}
